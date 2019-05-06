eb4020ebe2f580e8261c0d6d2dec545da4c1604e
zuzannagasior <zuzanna.gasior9@gmail.com>
2019-05-06 21:57:08 +0200

    Updates

---
+++ precache-manifest.1cb850ca5e1c1f7717c0e7750015eced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba82dd883a113e2638d7b5cce27f11e",
+    "url": "/fullofletters/index.html"
+  },
+  {
+    "revision": "c7c328fc850168ab996e",
+    "url": "/fullofletters/static/css/main.16cffd0b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/fullofletters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "c7c328fc850168ab996e",
+    "url": "/fullofletters/static/js/main.cb03a316.chunk.js"
+  },
+  {
+    "revision": "541c327e8fd49ad7fc8a",
+    "url": "/fullofletters/static/js/runtime~main.a275a2e9.js"
+  }
+]);
